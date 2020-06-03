8b5aa53a15e6e447cd5c3240ceca0e6b0cb984ea
luanlipperthoffmann <luan.lippert91@rede.ulbra.br>
2020-06-03 19:08:45 -0300

    TDE IDW calculadora com js

--- A.D.S/2020-01/IDW/Avaliacao_Exericicios/Calculator (TDE  JS)/assets/js/script.js
+++ A.D.S/2020-01/IDW/Avaliacao_Exericicios/Calculator (TDE  JS)/assets/js/script.js
@@ -1,5 +1,3 @@
-//tag para colocar um alerta na pagina!
-alert("CALCULATOR 1.0v");
-
 //tag para criar uma linha no js dentro da pagina
-document.writeln(CALCULADORA RAIZ);
+document.writeln("CALCULADORA");
+
