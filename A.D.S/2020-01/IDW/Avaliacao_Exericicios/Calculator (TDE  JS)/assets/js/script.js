0926b3d465067ee6f507d66b4b8495320fe2e6a7
luanlipperthoffmann <luan.lippert91@rede.ulbra.br>
2020-06-09 18:11:44 -0300

    TDE calculadora finalizada

--- A.D.S/2020-01/IDW/Avaliacao_Exericicios/Calculator (TDE  JS)/assets/js/script.js
+++ A.D.S/2020-01/IDW/Avaliacao_Exericicios/Calculator (TDE  JS)/assets/js/script.js
@@ -1,6 +1,7 @@
 var displayBuffer = "";
 var numero = "";
 var operacao = [undefined,undefined,undefined];
+var resultado = undefined;
 
 function pressNum(num){
     numero =numero.concat(num.innerHTML);
@@ -8,15 +9,49 @@ function pressNum(num){
     showDisplay(num.innerHTML); 
 }
 
+//neste if o sinal da operação so podera ser selecionado e só aparecerá na tela uma vez
 function pressOperator(op){
-    operacao[0] = numero;
-    operacao[1] = op.innerHTML;
-    showDisplay(op.innerHTML);
+    if(operacao[1] == undefined){
+        operacao[0] = numero;
+        operacao[1] = op.innerHTML;
+        showDisplay(op.innerHTML);
+        numero = "";
+    }
+    
+}
+
+function pressResult(){
+    operacao[2] = numero;
+    var keepResultado;
+    switch(operacao[1]){
+        case '/' :
+            resultado = Number (operacao [0]) / Number (operacao [2]);
+            break;
+        case '*':
+            resultado = Number (operacao [0]) * Number (operacao [2]);
+            break;
+        case '+':
+            resultado = Number (operacao [0]) + Number (operacao [2]);
+            break;
+        case '-':
+            resultado = Number (operacao [0]) - Number (operacao [2]);
+            break;
+        case '%':
+            resultado = Number (operacao [0]) % Number (operacao [2]);
+            break;
+    }
+    keepResultado = resultado;
+    clearDisplay();
+    showDisplay(resultado);
+    clearMemory();
+    numero = keepResultado;
 
 }
 
 function clearMemory(){
     numero = "";
+    operacao = [undefined, undefined,undefined];
+    resultado= undefined;
 }
 
 function clearDisplay(){
