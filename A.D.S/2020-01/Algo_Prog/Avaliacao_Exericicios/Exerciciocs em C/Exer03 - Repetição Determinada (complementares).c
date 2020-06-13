e7a8ca1344876274625c4eae69cae9a6d057b887
luanlipperthoffmann <luan.lippert91@rede.ulbra.br>
2020-06-13 20:12:36 -0300

    ATT organizando pastas e arquivos nos repositórios

--- A.D.S/2020-01/Algo_Prog/Avaliacao_Exericicios/Exerciciocs em C/Exer03 - Repetição Determinada (complementares).c
+++
@@ -1,8 +0,0 @@
-/******************************************************************************
-3. Faça uma variação para o algoritmo do exercício 1:
-" calculando a média de notas finais dos alunos(media
-da turma).
-" contando quantos alunos tem a média maior ou igual
-a 7,0
-*******************************************************************************/
-K
